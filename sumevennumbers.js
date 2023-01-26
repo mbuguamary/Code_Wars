3cf954c2c74f2bbd4b62a3e133a5f2f51e178138
mbuguamary <mary.wairimu01@student.moringaschool.com>
2023-01-26 14:06:06 +0300

    add

--- sumevennumbers.js
+++ sumevennumbers.js
@@ -3,8 +3,9 @@ let numStr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sumEvens = (numStr) => {
   let sum = 0;
   for (let i = 0; i < numStr.length; i++) {
-    if (numStr[i] % 2 === 0) {
-    sum = sum + numStr[i];
+    if (numStr[i] % 2 === 0 && numStr[i]>sum) {
+    // sum = sum + numStr[i];
+      sum=numStr[i]
     }
   }
   return sum;
